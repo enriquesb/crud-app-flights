59ab2561fefeaecf3d360bee75ba3c3f83794376
enriquesb <enriquesb1@gmail.com>
2023-02-03 21:35:23 -0500

    Add logic to add new flight to database

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const mysql = require("mysql2");
+const bodyParser = require("body-parser");
 
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 connection.connect();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   connection.query("SELECT * FROM flights", function (err, rows, fields) {
     if (err) throw err;
     res.render("index", { dbValues: rows });
-    //console.log(rows[0].departure);
-    //console.log(typeof rows);
+  });
+});
+
+app.post("/new_flight", (req, res) => {
+  console.log(req.body);
+  sql = `INSERT INTO flights (origin, destination, price, airline, departure_date)VALUES ("${req.body.origin}", "${req.body.destination}", ${req.body.price}, "${req.body.airline}", "${req.body.date}");`;
+  //console.log(sql);
+  connection.query(sql, function (err, rows, fields) {
+    if (err) throw err;
+    res.redirect("/");
   });
 });
 
